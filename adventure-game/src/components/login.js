123a653f89b2ae3dae8c15d6d4dceadd41b8afd9
Sahar Jafari <shr.jafari@gmail.com>
2021-04-02 14:30:36 -0400

    login functionality

--- adventure-game/src/components/login.js
+++ adventure-game/src/components/login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -6,16 +7,46 @@ const Login = () => {
     password: "",
   });
 
+  const handleChange = (e, type) => {
+    setCredentials({ ...credentials, [type]: e.target.value });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: "https://shr-adv-project.herokuapp.com/api/login/",
+      data: credentials,
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleLogin}>
         <label>
           Username:
-          <input type="text" name="username" value={credentials.username} />
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            onChange={(e) => handleChange(e, "username")}
+          />
         </label>
         <label>
           Password:
-          <input type="text" name="password" value={credentials.password} />
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={(e) => handleChange(e, "password")}
+          />
         </label>
         <input type="submit" value="Login" />
       </form>
